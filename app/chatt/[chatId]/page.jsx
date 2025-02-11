51d243bfeee55212d686ff0b0c70e59b262e33b5
SiddDevZ <epic.siddarth@gmail.com>
2025-02-11 16:43:04 +0530

    Removed old chat

--- app/chatt/[chatId]/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { useParams } from 'next/navigation';
-import ChatPage from '../page';
-
-export default function DynamicChatPage() {
-  const params = useParams();
-  return <ChatPage key={params.chatId} />;
-}
